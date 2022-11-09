e5a494cd1ebbbe2a4ba0118cde28495976cbb2ee
GABRIEL-KOLLER <gabrielkoller.2@gmail.com>
2022-11-08 23:11:53 -0300

    nlw mobile react native

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme';
+import { AuthContextProvider } from './src/contexts/AuthContext';
+import { Routes } from './src/routes';
+import React from 'react';
+
+export default function App() {
+
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <AuthContextProvider children={undefined}>
+        <StatusBar 
+          barStyle='light-content'
+          backgroundColor="transparent"
+          translucent
+          />
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContextProvider>
+    </NativeBaseProvider>
+  );
+}
